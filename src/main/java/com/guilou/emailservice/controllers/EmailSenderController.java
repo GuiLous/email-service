eaad9170153d64453b4208966373f887b2bcf61d
GuiLou <guilhermesilva@acad.ifma.edu.br>
2023-12-07 09:46:19 -0300

    create controller

---
+++ src/main/java/com/guilou/emailservice/controllers/EmailSenderController.java
@@ -0,0 +1,32 @@
+package com.guilou.emailservice.controllers;
+
+import com.guilou.emailservice.application.EmailSenderService;
+import com.guilou.emailservice.core.EmailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+public class EmailSenderController {
+    private final EmailSenderService emailSenderService;
+
+    @Autowired
+    public EmailSenderController(EmailSenderService emailService) {
+        this.emailSenderService = emailService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        try {
+            this.emailSenderService.sendEmail(request.to(), request.subject(), request.body());
+            return ResponseEntity.ok("Email sent successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error sending email: " + e.getMessage());
+        }
+    }
+
+}
